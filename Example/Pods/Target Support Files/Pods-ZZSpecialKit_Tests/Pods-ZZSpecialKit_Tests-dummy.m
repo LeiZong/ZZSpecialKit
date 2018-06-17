d00ba7e36f699dbeb7ab3cc9a1cc82515e8240a6
Zong Lei <leasingzong@gmail.com>
2018-06-17 13:25:08 +0800

    init ZZSpecialKit

---
+++ Example/Pods/Target Support Files/Pods-ZZSpecialKit_Tests/Pods-ZZSpecialKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZZSpecialKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZZSpecialKit_Tests
+@end
---
+++ Example/Pods/Target Support Files/ZZSpecialKit/ZZSpecialKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZZSpecialKit : NSObject
+@end
+@implementation PodsDummy_ZZSpecialKit
+@end
